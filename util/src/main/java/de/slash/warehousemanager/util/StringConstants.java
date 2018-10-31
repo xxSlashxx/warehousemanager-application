f50e5a808cd662830a43d6bf9c1b8205c211221a
xxSlashxx <roland.speckmeier@gmx.de>
2018-10-31 22:09:36 +0100

    Added constants for the side menu buttons

--- util/src/main/java/de/slash/warehousemanager/util/StringConstants.java
+++ util/src/main/java/de/slash/warehousemanager/util/StringConstants.java
@@ -9,4 +9,18 @@ public class StringConstants
     public static final String APP_NAME = "Warehousemanager";
 
     public static final String FONT_NAME = "Roboto Condensed";
+
+    public static final String SIDE_MENU_HEADER = "Menu";
+
+    public static final String SIDE_MENU_WAREHOUSES = "Warehouses";
+
+    public static final String SIDE_MENU_STORAGE_BINS = "Storage Bins";
+
+    public static final String SIDE_MENU_HANDLING_UNITS = "Handling Units";
+
+    public static final String SIDE_MENU_ARTICLES = "Articles";
+
+    public static final String SIDE_MENU_ORDERS = "Orders";
+
+    public static final String SIDE_MENU_CUSTOMERS = "Customers";
 }
